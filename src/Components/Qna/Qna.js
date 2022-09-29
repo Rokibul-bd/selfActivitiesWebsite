6528b0e973d74fc6b165b10b624f42c158eb979e
Rokibul-bd <mdrokbiulislamcmt176@gmail.com>
2022-09-29 13:26:29 +0600

    added quanstions components

--- src/Components/Qna/Qna.js
+++ src/Components/Qna/Qna.js
@@ -4,9 +4,9 @@ import './Qna.css'
 const Qna = () => {
     return (
         <div className='qna-container'>
-            <Quantions quantion="Whats your Name" ans="My Name Is Rakib"></Quantions>
-            <Quantions quantion="How old are you" ans="I am 22 years old"></Quantions>
-            <Quantions quantion="where are live in" ans="I am live in Thakurgaon"></Quantions>
+            <Quantions quantion="How Does React Work" ans="Instead of manipulating the browser's DOM directly, React creates a virtual DOM in memory, where it does all the necessary manipulating, before making the changes in the browser DOM."></Quantions>
+            <Quantions quantion="Deffarance between props and state" ans="React is an opensource JavaScript library that offers a visual overview of the JavaScript architecture for conventional MVC. React promises programmers a model in which substrates cannot directly influence enclosing components—data are downstream, data changes in HTML are efficiently modified, and the DOM is abstracted to boost performance using Virtual DOM."></Quantions>
+            <Quantions quantion="what wark useEffact without data load" ans="Before we continue, we should summarize the main concepts you’ll need to understand to master useEffect. Throughout the article, I will highlight the different aspects in great detail."></Quantions>
         </div>
     );
 };
